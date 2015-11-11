ea323fdd9e00163dda2d46d7bb7354e5394b1a72
Aurélien <aurel.andre@yahoo.fr>
2015-11-11 18:35:44 +0100

    Ecouteurs et affichage commande des déplacements

--- pacman/modele/Mainclass.java
+++ pacman/modele/Mainclass.java
@@ -1,11 +1,13 @@
 package pacman.modele;
 
+import java.util.Scanner;
+
 import pacman.carte.Labyrinthe;
 import pacman.personnages.Pacman;
 
-public class Mainclass {
+public class MainClass {
 	
-	public Mainclass(){
+	public MainClass(){
 		Pacman pacman = new Pacman(0, 0);
 		Labyrinthe laby = new Labyrinthe(5, 5, pacman);
 		Modele m = new Modele(laby);
@@ -13,12 +15,47 @@ public class Mainclass {
 		boolean exit = false;
 		
 		while(!exit){
-			
+			System.out.println("Ecrire commande (L/R/U/D/S) \n");
+			Scanner sc = new Scanner(System.in);
+			String entree = sc.nextLine();
+			switch(entree){
+				case "L":
+					if(!m.murAGauche()){
+						m.deplacerPacmanGauche();
+						System.out.println("L");
+					}
+					break;
+				case "R":
+					if(!m.murADroite()){
+						m.deplacerPacmanDroite();
+						System.out.println("R");
+					}
+					break;
+				case "U":
+					if(!m.murEnHaut()){
+						m.deplacerPacmanHaut();
+						System.out.println("U");
+					}
+					break;
+				case "D":
+					if(!m.murEnBas()){
+						m.deplacerPacmanBas();
+						System.out.println("D");
+					}
+					break;
+				case "S":
+					System.out.println("S");
+					break;
+				default:
+					break;
+			}
+			sc.close();
 		}
 	}
 
 	public static void main(String[] args) {
-		new Mainclass();
+		
+		new MainClass();
 	}
 	
 }
