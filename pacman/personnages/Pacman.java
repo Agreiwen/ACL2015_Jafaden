feac94294d10f4dade673b10eda96744148d5b66
BallyB <ajeanne25@gmail.com>
2015-11-11 17:43:07 +0100

    Mouvement et methodes Pacman

--- pacman/personnages/Pacman.java
+++ pacman/personnages/Pacman.java
@@ -6,9 +6,41 @@ public class Pacman {
 	private int coordonneex;
 	private int coordonneey;
 	
-	public Pacman(){
+	public Pacman(int x, int y){
+		this.coordonneex = x;
+		this.coordonneey = y;
 		
 	}
 	
+	public void deplacerGauche(){
+		coordonneex--;
+	}
+	
+	public void deplacerDroite(){
+		coordonneex++;
+	}
+	public void deplacerHaut(){
+		coordonneey++;
+	}
+	public void deplacerBas(){
+		coordonneey--;
+	}
+	
+	public int getCoordonneex() {
+		return coordonneex;
+	}
+
+	public void setCoordonneex(int coordonneex) {
+		this.coordonneex = coordonneex;
+	}
+
+	public int getCoordonneey() {
+		return coordonneey;
+	}
+
+	public void setCoordonneey(int coordonneey) {
+		this.coordonneey = coordonneey;
+	}
+
 	
 }
