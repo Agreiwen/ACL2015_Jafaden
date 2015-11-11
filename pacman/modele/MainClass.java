bbb6cb1f331657b0627720eaaa07df8590b25067
BallyB <ajeanne25@gmail.com>
2015-11-11 19:28:07 +0100

    Affichage coordonnee

--- pacman/modele/MainClass.java
+++ pacman/modele/MainClass.java
@@ -17,39 +17,42 @@ public class MainClass {
 			System.out.println("Ecrire commande (L/R/U/D/S) \n");
 			Scanner sc = new Scanner(System.in);
 			String entree = sc.nextLine();
-			
+			System.out.println("Pacman en ("+m.getCoordonneeLargeur()+","+m.getCoordonneeHauteur()+")\n");
+
 			switch(entree){
 				case "L":
 					
 					if(!m.murAGauche()){
 						m.deplacerPacmanGauche();
 					}
-					
-					System.out.println(laby.toString());
+					//System.out.println("Pacman en ("+m.getCoordonneeLargeur()+","+m.getCoordonneeHauteur()+")\n");
+
+					//System.out.println(laby.toString());
 					break;
 				case "R":
 					
 					if(!m.murADroite()){
 						m.deplacerPacmanDroite();
 					}
-					System.out.println(laby.toString());
+					
+					//System.out.println(laby.toString());
 					break;
 				case "U":
 					
 					if(!m.murEnHaut()){
 						m.deplacerPacmanHaut();
 					}
-					System.out.println(laby.toString());
+					//System.out.println(laby.toString());
 					break;
 				case "D":
 					
 					if(!m.murEnBas()){
 						m.deplacerPacmanBas();
 					}
-					System.out.println(laby.toString());
+					//System.out.println(laby.toString());
 					break;
 				case "S":
-					System.out.println(laby.toString());
+					//System.out.println(laby.toString());
 					break;
 				default:
 					break;
