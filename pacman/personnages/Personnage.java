6688206f4388267170d7c311b84b276efeefeaed
Thibault Delavelle <delavell2u@fst-ats-319-l19.fst.site.univ-lorraine.fr>
2015-11-19 11:07:55 +0100

    Ajout classe au dessus de Pacman, nommée Personnage

---
+++ pacman/personnages/Personnage.java
@@ -0,0 +1,51 @@
+package pacman.personnages;
+
+public abstract class Personnage {
+
+	protected static int largeur;
+	protected static int hauteur;
+	
+	
+	public Personnage(int largeur, int hauteur){
+		this.largeur=largeur;
+		this.hauteur=hauteur;
+	}
+
+
+	public int getLargeur() {
+		return largeur;
+	}
+
+
+	public void setLargeur(int largeur) {
+		this.largeur = largeur;
+	}
+
+
+	public int getHauteur() {
+		return hauteur;
+	}
+
+
+	public void setHauteur(int hauteur) {
+		this.hauteur = hauteur;
+	}
+	
+	
+	/* Methodes de deplacement */
+	
+	public void deplacerGauche(){
+		largeur--;
+	}
+	
+	public void deplacerDroite(){
+		largeur++;
+	}
+	public void deplacerHaut(){
+		hauteur++;
+	}
+	public void deplacerBas(){
+		hauteur--;
+	}
+	
+}
