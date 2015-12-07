4ec0a59462ae14bb33083e7089c7e31aa2928b4f
Aurélien <aurel.andre@yahoo.fr>
2015-12-07 23:36:52 +0100

    Correction du lag - affichage des textures

--- pacman/graphique/texture/Texture.java
+++ pacman/graphique/texture/Texture.java
@@ -7,40 +7,43 @@ import javax.swing.ImageIcon;
 
 public class Texture {
 	
-	public ArrayList<Image> images;
+	public ArrayList<Image> imagesPacman;
+	public Image mur;
+	public Image passage;
+	public Image tresor;
 	
 	public Texture(){
-		images = new ArrayList<Image>();
+		mur = new ImageIcon(this.getClass().getResource("../texture/mur.png")).getImage();
+		passage = new ImageIcon(this.getClass().getResource("../texture/passage.png")).getImage();
+		tresor = new ImageIcon(this.getClass().getResource("../texture/coffre.png")).getImage();
+		imagesPacman = new ArrayList<Image>();
 		ajoutDesImages();
 	}
 	
 	public void ajoutDesImages(){
 		for(int i=1; i<=16; i++){
 			Image pacman = new ImageIcon(this.getClass().getResource("../texture/pacman"+i+".png")).getImage();
-			images.add(pacman);
+			imagesPacman.add(pacman);
 		}
 		for(int i=1; i<=5; i++){
 			Image fantome = new ImageIcon(this.getClass().getResource("../texture/fantome"+i+".png")).getImage();
-			images.add(fantome);
+			imagesPacman.add(fantome);
 		}
 	}
 	
-	public Image getTextureMur(){
-		Image mur = new ImageIcon(this.getClass().getResource("../texture/mur.png")).getImage();
-		return mur;
+	public Image getTexture(int i){
+		return imagesPacman.get(i);
 	}
 	
-	public Image getTexture(int i){
-		return images.get(i);
+	public Image getTextureMur(){
+		return mur;
 	}
 
 	public Image getTextureCaseLibre() {
-		Image mur = new ImageIcon(this.getClass().getResource("../texture/passage.png")).getImage();
-		return mur;
+		return passage;
 	}
 	
 	public Image getTextureCaseTresor() {
-		Image mur = new ImageIcon(this.getClass().getResource("../texture/coffre.png")).getImage();
-		return mur;
+		return tresor;
 	}
 }
